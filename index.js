b13d20eb24845dedc61d70f0c9db0d339a8efd1c
paibamboo <gavinsitti@hotmail.com>
2019-03-28 16:18:20 +0700

    WIP: start sprint

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
+const inquirer = require('./lib/inquirer');
+const git = require('./lib/git');
 const github = require('./lib/github');
 const program = require('commander');
 program
-  .command('generate-token')
-  .description('generate Github personal access token to be used by gitflow')
+  .command('generate token')
+  .description('Generate Github personal access token to be used by gitflow')
   .action(async () => {
     try {
       await github.generateToken();
@@ -13,11 +15,47 @@ program
     }
   });
 program
-  .command('remove-token')
-  .description('remove Github personal access token from your local machine')
+  .command('get config')
+  .description('Set default project and sprint number')
+  .action(() => {
+    try {
+      console.log(git.getConfig(process.cwd()));
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+program
+  .command('remove token')
+  .description('Remove Github personal access token from your local machine')
   .action(async () => {
     try {
       await github.removeToken();
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+program
+  .command('set config')
+  .description('Set default project and sprint number')
+  .action(async () => {
+    try {
+      const config = await inquirer.askProjectNameAndSprintNumber({projectName: null, sprintNumber: null});
+      git.setConfig(process.cwd(), config);
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+program
+  .command('start sprint')
+  .description('Create new sprint branch')
+  .action(async () => {
+    try {
+      const isGitDirectory = await git.isGitDirectory();
+      if (!isGitDirectory) {
+        console.log('You are not in a git directory');
+        return;
+      }
+      await git.startSprint(git.getConfig(process.cwd()));
     } catch (e) {
       console.log(e.message);
     }
