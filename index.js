0ed5a58972120f5921b0ddc06b270ef603d65ddb
paibamboo <gavinsitti@hotmail.com>
2019-06-17 17:14:11 +0700

    fix: version commands read package.json from wrong dir

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const path = require('path');
 const inquirer = require('./lib/inquirer');
 const git = require('./lib/git');
 const github = require('./lib/github');
 const program = require('commander');
 
-program.version(JSON.parse(fs.readFileSync('./package.json'))['version'] || 'test-mode', '-v, --version');
+program.version(
+  JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json')))['version'] || 'test-mode', '-v, --version'
+);
 
 program
   .command('finish-feature')
